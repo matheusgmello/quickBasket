f996e9a12d67e071265896cd6fb90e5ae79f76e4
matheus <mathgfmello@gmail.com>
2026-01-22 11:30:13 -0300

    feat:  adicionado cache ProductService e implementado Serializable no response.

--- src/main/java/dev/matheus/basketservice/service/ProductService.java
+++ src/main/java/dev/matheus/basketservice/service/ProductService.java
@@ -3,24 +3,29 @@ package dev.matheus.basketservice.service;
 import dev.matheus.basketservice.client.PlatziStoreClient;
 import dev.matheus.basketservice.client.response.PlatziProductResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
 
     private final PlatziStoreClient platziStoreClient;
 
-
-
+    @Cacheable(value = "products")
     public List<PlatziProductResponse> getAllProducts() {
+        log.info("Setting all products");
         return platziStoreClient.getAllProducts();
     }
 
-    public PlatziProductResponse getProductById(Long id) {
-        return platziStoreClient.getProductById(id);
+    @Cacheable(value = "product", key = "#productId")
+    public PlatziProductResponse getProductById(Long productId) {
+        log.info("Setting product by id: {}", productId);
+        return platziStoreClient.getProductById(productId);
     }
 
 }
